90c02d96a8a74688dba3a88e606152d3300727a6
Wolfenek <wolfenek@gmail.com>
2018-11-16 22:06:51 +0100

    added more scenes to section 3. Project almost complete

--- js/wow-bgs.js
+++ js/wow-bgs.js
@@ -1,13 +1,10 @@
-const scenes = ['linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(scenes/scene1.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(scenes/scene2.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(scenes/scene3.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(scenes/scene4.jpg)'];
-        
-        let currentImg = ''
+const scenes = ['linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(scenes/scene1.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(scenes/scene2.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(scenes/scene3.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(scenes/scene4.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(scenes/scene5.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(scenes/scene6.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(scenes/scene7.jpg)', 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(scenes/scene8.jpg)'];
+    
+//Go through the array + add FadeOut effect
+let scIndex = 0;
+let tauren = document.getElementById('bg-switch');
 
-        function randomScene() {
-              const shuffle = Math.floor(Math.random() * (scenes.length));
-              if (scenes[shuffle] === currentImg) {
-                randomScene()
-              } else {
-                document.getElementById('bg-switch').style.backgroundImage = scenes[shuffle];
-                currentImg = scenes[shuffle];
-              }
-            };
+function newScene() {
+    tauren.style.backgroundImage = scenes[scIndex];
+    scIndex = (scIndex + 1)%(scenes.length);
+};
