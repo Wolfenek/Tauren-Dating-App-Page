70cc1484ea70b97386eb813ff08766319c2177e7
Wolfenek <wolfenek@gmail.com>
2019-05-27 11:03:00 +0200

    simplified jQuery code in wow-icons

--- js/wow-icons.js
+++ js/wow-icons.js
@@ -11,21 +11,8 @@ $(function() {
       });
   }
   // 1st set of icons:
-  $("#warrior").on("click", twistTheIcon);
+  $(".wow-class-icons img").on("click", twistTheIcon);
 
-  $("#paladin").on("click", twistTheIcon);
-
-  $("#dk").on("click", twistTheIcon);
-
-  $("#hunter").on("click", twistTheIcon);
-
-  $("#shaman").on("click", twistTheIcon);
-
-  $("#druid").on("click", twistTheIcon);
-
-  $("#monk").on("click", twistTheIcon);
-
-  $("#priest").on("click", twistTheIcon);
   // function that scales the icon. Passed as a parameter to the 'on' method in the examples below.
   function scaleTheIcon() {
     $(this)
@@ -36,11 +23,5 @@ $(function() {
   }
 
   // 2nd set of icons:
-  $("#t-male").on("click", scaleTheIcon);
-
-  $("#t-female").on("click", scaleTheIcon);
-
-  $("#h-male").on("click", scaleTheIcon);
-
-  $("#h-female").on("click", scaleTheIcon);
+  $(".wow-tauren-icons img").on("click", scaleTheIcon);
 });
